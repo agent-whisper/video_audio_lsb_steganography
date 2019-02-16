1a3baf2a2b056ca99594670be2dee3e1943cb441
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-02-16 12:31:17 +0700

    Added file input dialog and execute button

--- app/pages/video.py
+++ app/pages/video.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.filedialog as tkfd
 
 class VideoInsertionForm(tk.Frame):
     def __init__(self, master):
@@ -19,7 +20,43 @@ class VideoInsertionForm(tk.Frame):
 
         # Judul Halaman
         tk.Label(self, text='Steganografi Video', font='none 24 bold').grid(row=TITLE_ROW, columnspan=2, sticky=tk.W+tk.E)
-        
+
+        # Input lokasi file cover
+        self.cover_file_dir = tk.StringVar()
+        self.cover_file_dir.set('')
+        cv_dialog_frame = tk.Frame(self)
+        cv_dialog_frame.grid(row=COVER_FILE_ROW, column=0, columnspan=2, sticky=tk.W+tk.E)
+        tk.Label(master=cv_dialog_frame, text='Cover Video:').grid(row=0, sticky=tk.W)
+        cv_input_label = tk.Label(master=cv_dialog_frame, textvariable=self.cover_file_dir)
+        cv_input_label.grid(row=1, sticky=tk.W)
+        pick_cv_button = tk.Button(
+            master=cv_dialog_frame,
+            text='Pilih',
+            command = lambda: self.load_video_file(),
+        )
+        pick_cv_button.grid(row=2, column=0, sticky=tk.W)
+        preview_cv_button = tk.Button(
+            master=cv_dialog_frame,
+            text='Buka Video',
+            # command=,
+        )
+        preview_cv_button.grid(row=2, column=1, sticky=tk.W)
+
+        # Input lokasi file rahasia
+        self.secret_message_dir = tk.StringVar()
+        self.secret_message_dir.set('')
+        msg_dialog_frame = tk.Frame(self)
+        msg_dialog_frame.grid(row=SECRET_MESSAGE_ROW, column=0, columnspan=2, sticky=tk.W+tk.E)
+        tk.Label(master=msg_dialog_frame, text='Berkas Rahasia:').grid(row=0, sticky=tk.W)
+        msg_input_label = tk.Label(master=msg_dialog_frame, textvariable=self.secret_message_dir)
+        msg_input_label.grid(row=1, sticky=tk.W)
+        pick_msg_button = tk.Button(
+            master=msg_dialog_frame,
+            text='Pilih',
+            command = lambda: self.load_secret_message(),
+        )
+        pick_msg_button.grid(row=2, sticky=tk.W)
+
         # Input Stegano Key
         key_label = tk.Label(self, text='Stegano Key:')
         key_label.grid(row=KEY_ENTRY_ROW, column=0, sticky=tk.W)
@@ -54,6 +91,16 @@ class VideoInsertionForm(tk.Frame):
             b.grid(row=row_offset, column=1, sticky=tk.W)
             row_offset += 1
 
-        # Tombol kembali
+        # Tombol Eksekusi dan kembali
+        execute_button = tk.Button(self, text='Eksekusi')
+        execute_button.grid(row=OPTIONS_ROW+1, column=0)
         return_button = tk.Button(self, text='Kembali', command=lambda: master.open_main_menu())
-        return_button.grid(row=OPTIONS_ROW+1, column=0, columnspan=2)
+        return_button.grid(row=OPTIONS_ROW+1, column=1)
+    
+    def load_video_file(self):
+        self.cover_file_dir.set(tkfd.askopenfilename(filetypes=(
+            (".AVI Videos", "*.avi"),
+        )))
+
+    def load_secret_message(self):
+        self.secret_message_dir.set(tkfd.askopenfilename())
