fa6c61e60fac708d135b7eb088c65aa345bbf2fb
Radiyya Dwisaputra <radiyyasaputra@gmail.com>
2019-02-17 10:55:56 +0700

    Add decryption class

--- Audio/decrypt.py
+++ Audio/decrypt.py
@@ -1,103 +1,105 @@
-# Use wave package (native to Python) for reading the received audio file
 import wave
 import random
 import base64
-song = wave.open("song_embedded.wav", mode='rb')
+import tools
 
-def split_array(seq, num):
-    out = []
-    last = 0
-
-    while last < len(seq):
-        out.append(seq[int(last):int(last + num)])
-        last += num
-    return out
-
-# Convert audio to byte array
-frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-
-def get_string(frame_bytes):
-    opt = 1
-    encrypted = bin(frame_bytes[0])[-1] == '1'
-    random_frames = bin(frame_bytes[1])[-1] == '1'
-    random_bytes = bin(frame_bytes[2])[-1] == '1'
-    opt = 2 if bin(frame_bytes[3])[-1] == '1' else 1
-
-    index = 0 
-    temp = ""
-    string = ""
-    mod_index = 8
-    if opt==2 :
-        mod_index = 4
-
-    # Extract the LSB of each byte
-    if opt==1:
-        extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-        extracted = split_array(extracted, 4)
-
-    # Extract the 2 LSB of each byte
-    if opt==2:
-        extracted = [bin(frame_bytes[i] & 3).lstrip('0b').rjust(2,'0') for i in range(len(frame_bytes))]
-        extracted = split_array(extracted, 4)
-
-    # random frame case
-    if random_frames:
-        random.seed(84)
-        frame_list = list(range(len(extracted)))
-        random.shuffle(frame_list)
-    else:
-        frame_list = range(len(extracted))
-
-    # random bytes case
-    if random_bytes:
-        random.seed(84)
-        byte_list = list(range(len(extracted[0])))
-        random.shuffle(byte_list)
-    else:
-        byte_list = range(len(extracted[0]))
-
-    for i in frame_list:
-        for j in byte_list:
-            if i!=0:
-                if index % mod_index != (mod_index - 1):
-                    temp += str(extracted[i][j])
-                else:
-                    temp += str(extracted[i][j])
-                    # print(temp)
-                    string += chr(int(temp,2))
-                    # print(string)
-                    temp = ""
-                index += 1
+class Decrypt:
+    # Read encrypted file as bytes
+    def read_encrypted_file(self, filename):
+        song = wave.open(filename, mode='rb')
+        frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+        return frame_bytes
     
-    return string
+    # Get LSB from file in string format
+    def get_lsb(self, frame_bytes,key):
+        
+        # Get sign from LSB
+        encrypted = bin(frame_bytes[0])[-1] == '1'
+        random_frames = bin(frame_bytes[1])[-1] == '1'
+        random_bytes = bin(frame_bytes[2])[-1] == '1'
 
-# Get the sign of message in audio
-def get_info_message(string):
-    length = ""
-    extension = ""
-    i = 0
-    while string[i] != '#':
-        length += string[i]
-        i += 1
-    i+=1
-    while string[i] != '#':
-        extension += string[i]
-        i += 1
+        # Opt is sign for amount of LSB that used
+        opt = 2 if bin(frame_bytes[3])[-1] == '1' else 1
 
-    return (int(length), extension)
+        index = 0 
+        temp = ""
+        string = ""
+        key = key.upper()
+        seed = 0
+        for i in key:
+            seed += ord(i)
 
-string = get_string(frame_bytes)
-length, ext = get_info_message(string)
-init = len(str(length)) + len(str(ext)) + 2
-decoded = string[init:init+length]
-bytes_file = decoded.encode('utf-8')
-bytes_file = base64.b64decode(bytes_file)
+        # Extract the LSB of each byte
+        if opt==1:
+            mod_index = 8
+            extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+            extracted = tools.split_array(extracted, 4)
 
-with open('result' + '.' + ext, 'wb') as fd:
-    fd.write(bytes_file)
+        # Extract the 2 LSB of each byte
+        if opt==2:
+            mod_index = 4
+            extracted = [bin(frame_bytes[i] & 3).lstrip('0b').rjust(2,'0') for i in range(len(frame_bytes))]
+            extracted = tools.split_array(extracted, 4)
 
+        # Handling random frame case
+        if random_frames:
+            random.seed(seed)
+            frame_list = list(range(len(extracted)))
+            random.shuffle(frame_list)
+        else:
+            frame_list = range(len(extracted))
 
-# Print the extracted text
-print("Decrypted!!")
-print('Your Message : ' +decoded)
-song.close()
+        # Handling random bytes case
+        if random_bytes:
+            random.seed(seed)
+            byte_list = list(range(len(extracted[0])))
+            random.shuffle(byte_list)
+        else:
+            byte_list = range(len(extracted[0]))
+
+        # Get all in string format
+        for i in frame_list:
+            for j in byte_list:
+                if i!=0:
+                    if index % mod_index != (mod_index - 1):
+                        temp += str(extracted[i][j])
+                    else:
+                        temp += str(extracted[i][j])
+                        string += chr(int(temp,2))
+                        temp = ""
+                    index += 1
+        
+        # Decrypt if needed
+        if encrypted:
+            string = tools.vigenere_extended_decryption(string,key)
+        return string
+
+    # Get the sign of message in audio (length, extension)
+    def get_info_message(self, string):
+        length = ""
+        extension = ""
+        i = 0
+
+        while string[i] != '#':
+            length += string[i]
+            i += 1
+
+        i+=1
+        while string[i] != '#':
+            extension += string[i]
+            i += 1
+
+        return (int(length), extension)
+    
+    # Print the message inside file
+    def print_message(self, length, ext, string, outputname):
+        init = len(str(length)) + len(str(ext)) + 2 #Get init index
+        decoded = string[init:init+length]
+
+        # Convert to bytes
+        bytes_file = decoded.encode('utf-8')
+        bytes_file = base64.b64decode(bytes_file)
+        
+        with open(outputname + '.' + ext, 'wb') as fd:
+            fd.write(bytes_file)
+
