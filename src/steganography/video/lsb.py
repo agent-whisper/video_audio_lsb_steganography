15806f334be968623c52e62b6350e023277ceaa2
FerdiGhozali <ferdi0106ghozali@gmail.com>
2019-02-17 15:31:10 +0700

    fix many thing about video lsb

--- src/steganography/video/lsb.py
+++ src/steganography/video/lsb.py
@@ -8,7 +8,6 @@ import numpy as np
 import src.steganography.video.utilities as utils
 
 BYTE_PER_PIXEL = 3
-LSM_BYTE = 1
 HIDE_TEMP_FOLDER = 'steganography_temp'
 HIDE_OUTPUT_FOLDER = 'steganography_output'
 EXTRACT_TEMP_FOLDER = 'steganoanalysis_temp'
@@ -24,7 +23,6 @@ def hide_secret(cover_video_dir, secret_msg_dir, key, output_file_name, lsm_byte
     if (secret_msg_dir[character_idx] == '.') :
       break
     message_extension = secret_msg_dir[character_idx] + message_extension
-  print(message_extension)
   # Load the secret message
   ord_bytes = []
   message = ''
@@ -37,22 +35,29 @@ def hide_secret(cover_video_dir, secret_msg_dir, key, output_file_name, lsm_byte
       message = message + chr(ord(word))
   message_in_bytes = ''.join('{0:08b}'.format(ord(x)) for x in ord_bytes)
 
+  len_message = len(message_in_bytes)
+  extra_message = str(len_message) + '|' + message_extension + '|'
+  # print(extra_message)
+  extra_message = ''.join('{0:08b}'.format(ord(x)) for x in extra_message)
+
+  message_in_bytes = extra_message + message_in_bytes
+
   # Generate seed from key
   seed_from_key = generate_seed(key)
 
   # Configuration setting
   image_index = 0
-  save_config(image_index, is_seq_frame, is_seq_pixel, HIDE_TEMP_FOLDER)
+  save_config(image_index, is_seq_frame, is_seq_pixel, lsm_byte, HIDE_TEMP_FOLDER)
 
   # Message size validation
-  max_information_per_image = info_image['width'] * info_image['height'] * BYTE_PER_PIXEL * LSM_BYTE
+  max_information_per_image = info_image['width'] * info_image['height'] * BYTE_PER_PIXEL * lsm_byte
   is_msg_too_long = (max_information_per_image * info_image['total_image']) < len(message_in_bytes)
   if (is_msg_too_long) :
     print('message too long')
     return {'result': 'failed'}
   
   # Checking execution requirements
-  required_pixel_count = math.ceil(len(message_in_bytes) / (BYTE_PER_PIXEL * LSM_BYTE))
+  required_pixel_count = math.ceil(len(message_in_bytes) / (BYTE_PER_PIXEL * lsm_byte))
   required_frame_count = math.ceil(required_pixel_count / (info_image['width'] * info_image['height']))
   required_pixel_count = int(required_pixel_count)
   required_frame_count = int(required_frame_count)
@@ -72,7 +77,7 @@ def hide_secret(cover_video_dir, secret_msg_dir, key, output_file_name, lsm_byte
   )
 
   # Start steganography process
-  apply_steganography(info_image, pixel_order, message_in_bytes, BYTE_PER_PIXEL, LSM_BYTE, HIDE_TEMP_FOLDER)
+  apply_steganography(info_image, pixel_order, message_in_bytes, BYTE_PER_PIXEL, lsm_byte, HIDE_TEMP_FOLDER)
   try :
     os.mkdir(HIDE_OUTPUT_FOLDER)
   except OSError:
@@ -90,8 +95,7 @@ def hide_secret(cover_video_dir, secret_msg_dir, key, output_file_name, lsm_byte
 def extract_secret(stegano_video_dir, key, output_file_name='video_extraction_result'):
   info_image = utils.video_to_image(stegano_video_dir, EXTRACT_TEMP_FOLDER)
   image_index = 0
-  is_seq_frame, is_seq_pixel = extract_config(image_index, EXTRACT_TEMP_FOLDER)
-
+  is_seq_frame, is_seq_pixel, lsm_byte = extract_config(image_index, EXTRACT_TEMP_FOLDER)
   seed_from_key = generate_seed(key)
 
   need_pixel = 240
@@ -102,12 +106,12 @@ def extract_secret(stegano_video_dir, key, output_file_name='video_extraction_re
   need_pixel = int(need_pixel)
   need_frame = int(need_frame)
   pixel_order = utils.generate_random_order_pixel(seed_from_key, need_pixel, need_frame, is_seq_frame, is_seq_pixel, pixel_range, frame_range, pixel_per_image)
-  ledcn_message, len_total_message, extension, range_message = get_extension_len_message(info_image, pixel_order, BYTE_PER_PIXEL, LSM_BYTE, EXTRACT_TEMP_FOLDER)
-  need_pixel = math.ceil(len_total_message / (BYTE_PER_PIXEL * LSM_BYTE))
+  len_message, len_total_message, extension, range_message = get_extension_len_message(info_image, pixel_order, BYTE_PER_PIXEL, lsm_byte, EXTRACT_TEMP_FOLDER)
+  need_pixel = math.ceil(len_total_message / (BYTE_PER_PIXEL * lsm_byte))
   
+  pixel_order = utils.generate_random_order_pixel(seed_from_key, need_pixel, need_frame, is_seq_frame, is_seq_pixel, pixel_range, frame_range, pixel_per_image)
   pixel_order = np.array(pixel_order)
-  message = apply_steganoanalytic(info_image, pixel_order, BYTE_PER_PIXEL, LSM_BYTE, range_message, EXTRACT_TEMP_FOLDER)
-  print(extension)
+  message = apply_steganoanalytic(info_image, pixel_order, BYTE_PER_PIXEL, lsm_byte, range_message, EXTRACT_TEMP_FOLDER)
   if (extension == 'plain') :
     plain = ''
     for i in range (0,len(message) // 8) :
@@ -199,15 +203,14 @@ def get_extension_len_message(info_image, pixel_order, byte_per_pixel, lsm_byte,
   len_message = 0
   xi = 1
   len_total_message = 0
+  extension_idx = 0
   for message_idx in range (0, len(pixel_order)*lsm_byte*byte_per_pixel,lsm_byte*byte_per_pixel) :
-    byte_idx = pixel_order[int(message_idx/(lsm_byte*byte_per_pixel))]		
+    byte_idx = pixel_order[int(message_idx/(lsm_byte*byte_per_pixel))]    
     pixel_each_img = info_image['width']*info_image['height']
     img_idx = byte_idx // pixel_each_img
     height_img = (byte_idx % pixel_each_img) // (info_image['width'])
     width_img = (byte_idx % pixel_each_img) % (info_image['width'])
     message = message + extract_lsb(img_idx, height_img, width_img, byte_per_pixel, lsm_byte, temp_folder)
-    
-    y = 0
     range_message = (0, 0)
     if (len(message) // 8 == xi) :
       xi += 1
@@ -218,17 +221,17 @@ def get_extension_len_message(info_image, pixel_order, byte_per_pixel, lsm_byte,
         plain = plain + chr(x)
       if (plain[-1] == '|') :
         if (is_extension) :
-          extension = plain[y:-1]
-          y = len(plain)
+          extension = plain[extension_idx:-1]
+          extension_idx = len(plain)
           is_extension = False
           len_total_message = len(plain) * 8 + len_message
         if (is_len) :
           len_message = int(plain[:-1])
-          y = len(plain)
+          extension_idx = len(plain)
           is_len = False
           is_extension = True
     if (not(is_extension) and not(is_len)) :
-      range_message = (y*8, (y*8)+len_message)
+      range_message = (extension_idx*8, (extension_idx*8)+len_message)
       break
   return(len_message, len_total_message, extension, range_message)
 
@@ -238,9 +241,10 @@ def extract_config(image_index, temp_folder) :
   byte_lsb = ['{0:08b}'.format(x) for x in int_lsb]
   frame_sequencial = ((byte_lsb[0])[-3] == '1')
   pixel_sequencial = ((byte_lsb[1])[-3] == '1')
-  return(frame_sequencial, pixel_sequencial)
+  lsm_byte = int(str((byte_lsb[2])[-3])) + 1
+  return(frame_sequencial, pixel_sequencial, lsm_byte)
 
-def save_config(image_index, frame_sequencial, pixel_sequencial, temp_folder) :
+def save_config(image_index, frame_sequencial, pixel_sequencial, lsm_byte, temp_folder) :
   image = cv2.imread(temp_folder + "/" + str(image_index) + ".png", 1 )
   int_lsb = image[0,0]
   byte_lsb = ['{0:08b}'.format(x) for x in int_lsb]
@@ -257,6 +261,14 @@ def save_config(image_index, frame_sequencial, pixel_sequencial, temp_folder) :
     byte_lsb[1] = byte_lsb[1] + '1' + third_byte
   else :
     byte_lsb[1] = byte_lsb[1] + '0' + third_byte
+  third_byte = byte_lsb[2][-2:]
+  byte_lsb[1] = byte_lsb[2][:-3]
+  if (lsm_byte == 2) :
+    byte_lsb[2] = byte_lsb[2] + '1' + third_byte
+  else :
+    byte_lsb[2] = byte_lsb[2] + '0' + third_byte
+
+
   int_lsb = tuple([int(x, 2) for x in byte_lsb])
 
   
